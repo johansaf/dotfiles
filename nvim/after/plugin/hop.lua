1192d1f4029d2ec469de1b49a9a5754f373ec43c
Johan Saf <johan@pwd.re>
2024-02-23 09:27:19 +0100

    [nvim] add keybinding for hop plugin
    <leader>h is now :HopWord

--- nvim/after/plugin/hop.lua
+++ nvim/after/plugin/hop.lua
@@ -12,3 +12,5 @@ end, { remap = true })
 vim.keymap.set("", "T", function()
   hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
 end, { remap = true })
+
+vim.keymap.set("n", "<leader>h", ":HopWord<enter>", { noremap = true })
