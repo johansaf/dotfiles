f6d61381ad4da752818859b98d44ffe01f3d85b0
Johan Saf <johan@pwd.re>
2024-04-10 08:11:26 +0200

    [nvim] change keybinding for fterm

--- nvim/after/plugin/fterm.lua
+++ nvim/after/plugin/fterm.lua
@@ -4,4 +4,4 @@ require("FTerm").setup({
 
 -- Example keybindings
 vim.keymap.set("n", "<leader>t", '<CMD>lua require("FTerm").toggle()<CR>')
-vim.keymap.set("t", "<leader>t", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+vim.keymap.set("t", "<ESC>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
